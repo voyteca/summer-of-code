06e902a8e193053659611a0f87b21f6f57eb908a
voyteca <44102689+voyteca@users.noreply.github.com>
2018-10-14 23:04:56 +0100

    Add files via upload

---
+++ 2018/Task6-Seating_shenanigans/task06.py
@@ -0,0 +1,25 @@
+#PART 1 & 2
+groups={} #dict of sets
+ID=0
+for l in open("06-friendships.txt"):
+    friends=set(l.split()) #make set of friends
+    isInGroup=[]
+    for key,group in groups.items():
+        if friends & group: #check if one of friends is already in any of the groups
+            if len(isInGroup)==0:
+                groups[key].update(friends) #if yes merge sets
+            isInGroup.append(key) #record which group has a friend
+
+    #if there is no record creat new group of friends
+    if len(isInGroup)==0:
+        groups[ID]=set(friends)
+        ID+=1
+    #or if a friends has been recorded more than once merge all these groups
+    elif len(isInGroup)>1:
+        for groupID in isInGroup[1:]:
+            groups[isInGroup[0]]=groups[isInGroup[0]].union(groups[groupID])
+            del groups[groupID]
+     
+print(len(groups))
+print(max([len(group) for  group in groups.values()]))
+
