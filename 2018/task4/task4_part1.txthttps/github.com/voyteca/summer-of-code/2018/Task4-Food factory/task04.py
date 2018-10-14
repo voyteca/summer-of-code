ec3fffec23870ce0ed45db22888cf33599a6ef8e
voyteca <44102689+voyteca@users.noreply.github.com>
2018-10-14 22:39:09 +0100

    Add files via upload

---
+++ 2018/task4/task4_part1.txthttps/github.com/voyteca/summer-of-code/2018/Task4-Food factory/task04.py
@@ -0,0 +1,27 @@
+#PART 1
+from functools import reduce
+print(reduce((lambda t, itemTime:t+itemTime),[int(l.split()[1]) for l in open("04-preparation.txt")]))
+
+#PART 2
+tasks={}
+tasksTimes={}
+
+def getTaskTime(task):
+    dTime=0
+    if task in tasksTimes:
+        return tasksTimes[task]
+    tasksTimes[task]=int(tasks[task][0])
+    for dependsOn in tasks[task][1:]:
+        dependsOnTime=getTaskTime(dependsOn)        
+        if dependsOnTime>dTime: dTime=dependsOnTime
+    tasksTimes[task]+=dTime
+    return  tasksTimes[task]
+
+for l in open("04-preparation.txt"):
+    taskAsList=l.strip().split()
+    tasks[taskAsList[0]]=taskAsList[1:]
+
+for task in tasks.keys():
+    getTaskTime(task)
+
+print(max(tasksTimes.values()))
