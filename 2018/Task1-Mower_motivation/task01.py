a652458c004e4499bda56259e41063f6ac821dfc
voyteca <44102689+voyteca@users.noreply.github.com>
2018-10-20 20:16:06 +0100

    Update task01.py

--- 2018/Task1-Mower_motivation/task01.py
+++ 2018/Task1-Mower_motivation/task01.py
@@ -1,31 +1,7 @@
-##counter=0
-##for l in open("01-mowmaster.txt"):
-##    if not l.startswith("#"):
-##        counter+=1
-##
-##print(counter)
+"""Solution to Task 1 of Summer of Code 2018"""
 
-##count=len([l for l in open("01-mowmaster.txt") if not l.startswith("#")])
-#print(len([l for l in open("01-mowmaster.txt") if not l.startswith("#")]))
-
-##mx=1
-##my=x=y=0
-##
-##for l in open("01-mowmaster.txt"):
-##    mxTemp=mx
-##    if l[0]=="C":       
-##        mx=0 if abs(mx)==1 else 1 if my==1 else -1
-##        my=0 if abs(my)==1 else -1 if mxTemp==1 else 1
-##    elif l[0]=="A":
-##        dxTemp=mx
-##        mx=0 if abs(mx)==1 else -1 if my==1 else 1
-##        my=0 if abs(my)==1 else 1 if mxTemp==1 else -1
-##
-##    elif l[0]=="F":
-##        x+=int(l[1:])*mx
-##        y+=int(l[1:])*my
-##
-##print(abs(x)+abs(y))
+__author__      = "Wojciech Czubak"
+__copyright__   = "Copyright 2018"
 
 rotation={'C':{0:1,1:2,2:3,3:0},'A':{0:3,1:0,2:1,3:2}}
 directions=[[1,0],[0,-1],[-1,0],[0,1]]
