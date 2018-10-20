f94f3fab6520590b10944f7f3e88f90b76381427
voyteca <44102689+voyteca@users.noreply.github.com>
2018-10-20 19:46:07 +0100

    Add files via upload

---
+++ 2018/Task7-Burger_flipping/task07.py
@@ -0,0 +1,23 @@
+#PART 1 & 2
+
+data=[l.split() for l in open("07-flips.txt")]
+sorters=set()
+
+for instructions in data[1:]:
+    #make burger list of lists, with boolean for the flip state
+    burgers=[[burger,True] for burger in data[0][1:]]
+    for inst in instructions[1:]:
+        i=int(inst)
+        burgers=burgers[:i][::-1]+burgers[i:] #rotate burgers        
+        for s in range(i):burgers[s][1]=not burgers[s][1] #flip burgers
+
+    #check if sorted   
+    if  all(int(burgers[i][0]) <= int(burgers[i+1][0]) for i in range(len(burgers)-1)):
+        sorters.add(instructions[0])
+        
+        if all(b[1] for b in burgers): #check if all are flipped back
+            print ('best sorter:',instructions[0])
+    
+print ('number of working sorters:',len(sorters))
+
+
